be8701e6226d104934bb8f14d6b29dccb92ae7df
Julian Hector <jhector@mozilla.com>
2014-09-27 06:17:51 +0200

    added function to print error message

--- shell.cpp
+++ shell.cpp
@@ -1,6 +1,13 @@
 #include <cstdio>
 #include <cstdlib>
+#include <unistd.h>
+#include <string.h>
 #include <stdint.h>
+
+void error(const char *msg)
+{
+    write(1, msg, strlen(msg));
+}
 
 int32_t main(int32_t argc, char *argv[])
 {
